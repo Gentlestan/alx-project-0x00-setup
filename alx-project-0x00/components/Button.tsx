8edcccb00e3df8feefd52bfaec8c3ef0e29ca28e
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-16 08:22:22 -0700

    updated buttonprop.ts button.tsx

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,7 @@
+import { ButtonProps } from "@/interfaces";
+
+const sizeClasses: Record<ButtonProps["size"], string> = {
+    small: "px-3 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-5 py-3 text-lg",
+};
