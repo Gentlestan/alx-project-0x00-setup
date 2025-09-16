a2e525ddbf03d45bfbee5dd28094ef609d5acda5
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-15 23:27:34 -0700

    add about.tsx landing.tsx

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,10 @@
+const About: React.FC = () => {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>About the Airbnb Clone</h1>
+      <p className="mt-3 text-lg">This is a clone of the Airbnb application built with Next.js.</p>
+    </main>
+  )
+}
+
+export default About;
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,12 @@
+
+
+const Landing: React.FC = () => {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Welcome to the Airbnb Landing Page</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
+  )
+}
+
+export default Landing;
