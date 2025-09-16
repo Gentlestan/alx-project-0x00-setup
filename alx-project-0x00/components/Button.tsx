b1d5d1bd039cc994e32ca1234dbc849c76f6b927
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-16 08:49:31 -0700

    updated buttonprop.ts button.tsx

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -5,3 +5,13 @@ const sizeClasses: Record<ButtonProps["size"], string> = {
     medium: "px-4 py-2 text-base",
     large: "px-5 py-3 text-lg",
 };
+
+
+const Button: React.FC<ButtonProps> = ({ title, size, shape }) => {
+    return (
+        <button className={`bg-blue-500 text-white ${sizeClasses[size]} ${shape}`}>
+            {title}
+        </button>
+    );
+};
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,15 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 const Landing: React.FC =  () => {
   return (
-    <div>
+    <div className=" flex flex-col h-screen justify-center items-center gap-5">
       <h1 className=" text-xl font-extralight">Landing Page</h1>
         <Card />
         <Card />
         <Card />
+      <Button title="Small Button" size="small" shape="rounded-sm" />
+        <Button title="Medium Button" size="medium" shape="rounded-md" />
+        <Button title="Large Button" size="large" shape="rounded-lg" />
     </div>
   )
 }
